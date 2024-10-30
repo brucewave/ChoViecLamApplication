e27b9a697fdc09bf76be49cdbd0f9b46409c1794
brucewave <longthanh.dev@gmail.com>
2024-10-30 22:39:33 +0700

    Init Capstone

---
+++ src/constants/appInfos.ts
@@ -0,0 +1,15 @@
+import {Dimensions} from 'react-native';
+
+export const appInfo = {
+  sizes: {
+    WIDTH: Dimensions.get('window').width,
+    HEIGHT: Dimensions.get('window').height,
+  },
+  // //Mang cong ty
+  // BASE_URL: 'http://192.168.110.15:3001',
+  //Mang rieng
+  BASE_URL: 'http://192.168.0.105:3001',
+
+
+  
+};
